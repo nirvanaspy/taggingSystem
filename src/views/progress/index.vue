ca0a3cacc75123d5ded521ca57fe97078fbf21dd
nirvanaspy <nirvana_spy@outlook.com>
2018-06-12 11:38:22 +0800

    普通用户只能看到自己的进度，管理员用户可以看到所有人的进度

--- src/views/progress/index.vue
+++ src/views/progress/index.vue
@@ -1,44 +1,79 @@
 <template>
-  <div class="app-container calendar-list-container">
-    <div class="filter-container">
-      <el-input @keyup.enter.native="handleFilter" style="width: 200px;" class="filter-item" placeholder="用户名" v-model="searchQuery">
-      </el-input>
+  <div>
+    <div class="app-container calendar-list-container" v-if="loginInfo.username == 'admin'">
+      <div class="filter-container">
+        <el-input @keyup.enter.native="handleFilter" style="width: 200px;" class="filter-item" placeholder="用户名" v-model="searchQuery">
+        </el-input>
 
-      <el-button class="filter-item" type="primary" v-waves icon="el-icon-search" @click="handleFilter">搜索</el-button>
+        <el-button class="filter-item" type="primary" v-waves icon="el-icon-search" @click="handleFilter">搜索</el-button>
+      </div>
+
+      <el-table :key='tableKey' :data="listA" v-loading="listLoading" element-loading-text="数据加载中" border fit highlight-current-row
+                :default-sort = "{prop: 'markFinishedNum', order: 'descending'}"
+                style="width: 100%"
+      >
+
+        <el-table-column label="用户名" min-width="100" prop="username" sortable>
+          <template slot-scope="scope">
+            <span>{{scope.row.userEntity.username}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="申请标注数" prop="markNum" sortable>
+          <template slot-scope="scope">
+            <span>{{scope.row.markNum}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="完成标注数" prop="markFinishedNum" sortable>
+          <template slot-scope="scope">
+            <span>{{scope.row.markFinishedNum}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="申请审阅数" prop="reviewNum" sortable>
+          <template slot-scope="scope">
+            <span>{{scope.row.reviewNum}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="完成审阅数" prop="reviewFinishedNum" sortable>
+          <template slot-scope="scope">
+            <span>{{scope.row.reviewFinishedNum}}</span>
+          </template>
+        </el-table-column>
+
+      </el-table>
     </div>
+    <div class="app-container calendar-list-container" v-else>
+      <el-table :data="listB" v-loading="listLoading" element-loading-text="数据加载中" border fit highlight-current-row
+                style="width: 100%"
+      >
 
-    <el-table :key='tableKey' :data="listA" v-loading="listLoading" element-loading-text="数据加载中" border fit highlight-current-row
-              :default-sort = "{prop: 'markFinishedNum', order: 'descending'}"
-              style="width: 100%"
-    >
+        <el-table-column label="用户名" min-width="100">
+          <template slot-scope="scope">
+            <span>{{scope.row.userEntity.username}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="申请标注数">
+          <template slot-scope="scope">
+            <span>{{scope.row.markNum}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="完成标注数">
+          <template slot-scope="scope">
+            <span>{{scope.row.markFinishedNum}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="申请审阅数">
+          <template slot-scope="scope">
+            <span>{{scope.row.reviewNum}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="完成审阅数">
+          <template slot-scope="scope">
+            <span>{{scope.row.reviewFinishedNum}}</span>
+          </template>
+        </el-table-column>
 
-      <el-table-column label="用户名" min-width="100" prop="username" sortable>
-        <template slot-scope="scope">
-          <span>{{scope.row.userEntity.username}}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="申请标注数" prop="markNum" sortable>
-        <template slot-scope="scope">
-          <span>{{scope.row.markNum}}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="完成标注数" prop="markFinishedNum" sortable>
-        <template slot-scope="scope">
-          <span>{{scope.row.markFinishedNum}}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="申请审阅数" prop="reviewNum" sortable>
-        <template slot-scope="scope">
-          <span>{{scope.row.reviewNum}}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="完成审阅数" prop="reviewFinishedNum" sortable>
-        <template slot-scope="scope">
-          <span>{{scope.row.reviewFinishedNum}}</span>
-        </template>
-      </el-table-column>
-
-    </el-table>
+      </el-table>
+    </div>
   </div>
 </template>
 
@@ -89,7 +124,13 @@
             /* console.log(item)*/
             return item.userEntity.username.toLowerCase().indexOf(self.searchQuery.toLowerCase()) !== -1;
           })
-
+        },
+        listB: function() {
+          const self = this
+          return self.list.filter(function (item) {
+            /* console.log(item)*/
+            return item.userEntity.username.toLowerCase().indexOf(self.loginInfo.username.toLowerCase()) !== -1;
+          })
         }
       }
     }
